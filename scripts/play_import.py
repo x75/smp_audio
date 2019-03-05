980d27131e9e1035dcabf32985ddd06ef373ad21
Oswald Berthold <oswald.berthold@gmail.com>
2019-03-05 12:50:34 +0100

    added try/except on load_libs / import_module

--- scripts/play_import.py
+++ scripts/play_import.py
@@ -16,7 +16,12 @@ approaches
 """
 
 import os
+import sys
+import inspect
+import pkgutil
 import importlib
+from importlib import import_module
+from pathlib import Path
 
 def load_from_file(filepath):
     import imp
@@ -40,13 +45,6 @@ def load_from_file(filepath):
 def load_lib_simple(action):
     action_ = importlib.import_module('actions.'+ action)
     return action_
-
-
-import sys
-import inspect
-import pkgutil
-from pathlib import Path
-from importlib import import_module
 
 def load_lib_simple_2():
     for (_, name, _) in pkgutil.iter_modules([os.path.dirname(__file__)]):
@@ -80,23 +78,43 @@ def load_lib_from_file():
     # by name later.
     sys.modules[module_name] = module
 
-def load_libs():
+def load_libs(**kwargs):
     """load_libs
 
     Load libraries for project
     - iter list of strings
     - return dict of names
     """
-    libs_to_load = {'librosa': None, 'aubio': None, 'essentia': None, 'essentia.standard': None, 'madmom': None}
+    if 'libs' in kwargs:
+        libs_to_load = kwargs['libs']
+    else:
+        # default libs to load
+        libs_to_load = {
+            'librosa': None, 'aubio': None, 'essentia': None,
+            'essentia.standard': None, 'madmom': None,
+            'blub': None,
+        }
+        
+    # libs loaded successfully
     libs_loaded = {}
+
+    # iterate libs to load
     for lib_to_load in libs_to_load:
         print('Trying to load {0}'.format(lib_to_load))
-        # FIXME: try/except
-        lib_loaded_ = import_module(lib_to_load)
-        print('Loaded {0} from {1}'.format(lib_loaded_, lib_to_load))
+        # try to load lib
+        try:
+            lib_loaded_ = import_module(lib_to_load)
+            print('Loaded {0} from {1}'.format(lib_loaded_, lib_to_load))
+        # catch fail non-fatal
+        except ImportError as err:
+            print('Failed to load {0} from {1}'.format(lib_loaded_, lib_to_load))
+            # option: at lib_to_load insert None 
+            # option: don't insert lib_to_load key
 
+        # debug
         print('Namespace    dir() = {0}'.format(dir()))
-        
+
+        # insert name into libs_loaded dict
         print('Inserting {0} into locals'.format(lib_to_load))
         # locals()[lib_to_load] = lib_loaded_
         libs_loaded[lib_to_load] = lib_loaded_

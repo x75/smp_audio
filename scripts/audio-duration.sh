a224b9d7ee91c397a0c982a046f5c94180e08c82
Oswald Berthold <oswald.berthold@gmail.com>
2021-01-16 21:04:25 +0100

    clean up scripts/, move all to playground except automain, autoapi, audio_duration. audio_dur clean up output formatting

--- scripts/audio-duration.sh
+++ scripts/audio-duration.sh
@@ -1,15 +1,27 @@
+#!/bin/bash
+
+# compute total duration of audio in a directory in milliseconds
+
+workdir="."
+if [ "$1" != "" ]; then
+    workdir=$1
+    echo "audio_duration directory ${workdir}"
+else
+    echo "audio_duration, no directory given, using default ${workdir}"
+fi
+
 shopt -s nullglob
 let playlist_duration_ms=0
-for song_file in *.{mp3,mp4,ogg,m4a,flac,wav,aiff,aif}; do
-	echo enter \#\#\#\#\#
-	echo     song file         $song_file
-	echo duration accumulated $playlist_duration_ms pre
-  	# playlist_duration_ms=$(expr $playlist_duration_ms + $(mediainfo --Inform="Audio;%Duration%" "$song_file"))
-	song_duration=$(mediainfo --Inform="Audio;%Duration%" "$song_file")
-	song_duration_int=${song_duration%.*}
-	echo     song duration dur $song_duration_int
-  	playlist_duration_ms=$(expr $playlist_duration_ms + $song_duration_int)
-	echo duration accumulated $playlist_duration_ms post
+for song_file in ${workdir}/*.{mp3,mp4,ogg,m4a,flac,wav,aiff,aif}; do
+    # echo enter \#\#\#\#\#
+    echo file `basename $song_file`
+    # echo duration accumulated $playlist_duration_ms pre
+    # playlist_duration_ms=$(expr $playlist_duration_ms + $(mediainfo --Inform="Audio;%Duration%" "$song_file"))
+    song_duration=$(mediainfo --Inform="Audio;%Duration%" "$song_file")
+    song_duration_int=${song_duration%.*}
+    echo "    duration file  = ${song_duration_int}ms"
+    playlist_duration_ms=$(expr $playlist_duration_ms + $song_duration_int)
+    echo "    duration total = ${playlist_duration_ms}ms"
 done
 shopt -u nullglob
 
